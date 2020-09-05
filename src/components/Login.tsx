ce171cf31a848ff93f971b3d66d6ae4f1d718a17
AkiraFukushima <h3.poteto@gmail.com>
2020-09-06 00:20:56 +0900

    Fix jest tests

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Platform} from 'react-native';
+// import {Platform} from 'react-native';
 import {WebView} from 'react-native-webview';
 import {
   DrawerScreenProps,
   DrawerNavigationProp,
 } from '@react-navigation/drawer';
-import CookieManager from '@react-native-community/cookies';
-import AsyncStorage from '@react-native-community/async-storage';
+// import CookieManager from '@react-native-community/cookies';
+// import AsyncStorage from '@react-native-community/async-storage';
 
 import {DrawerParam} from '@/navigations/drawer';
 
@@ -30,13 +30,13 @@ const handleWebViewNavigationStateChange = async (
   const {url} = newNavState;
   if (!url) return;
   if (url.includes('fascia.io/webviews/callback')) {
-    if (Platform.OS === 'ios') {
-      // For iOS
-      // Because WKWebView don't share cookie with other http requests.
-      // https://stackoverflow.com/questions/62057393/how-to-keep-last-web-session-active-in-react-native-webview
-      const cookies = await CookieManager.getAll(true);
-      await AsyncStorage.setItem('savedCookies', JSON.stringify(cookies));
-    }
+    /* if (Platform.OS === 'ios') {
+     *   // For iOS
+     *   // Because WKWebView don't share cookie with other http requests.
+     *   // https://stackoverflow.com/questions/62057393/how-to-keep-last-web-session-active-in-react-native-webview
+     *   const cookies = await CookieManager.getAll(true);
+     *   await AsyncStorage.setItem('savedCookies', JSON.stringify(cookies));
+     * } */
     navigation.navigate('Home', {
       screen: 'Projects',
     });
