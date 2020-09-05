b4b37c9560eb239b3f8ac131a09f814ac713a15c
AkiraFukushima <h3.poteto@gmail.com>
2020-09-05 22:23:38 +0900

    Add global type definition for react-hook-form

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+// For https://github.com/react-hook-form/react-hook-form/issues/2419
+// Original file is ./node_modules/react-hook-form/dist/global.d.ts
+
+interface HTMLOptionsCollection {}
+interface FileList {}
+interface MutationObserver {}
