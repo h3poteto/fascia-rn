8c9e95a8300e48bc4d9d121ce949f43cada508b1
AkiraFukushima <h3.poteto@gmail.com>
2020-09-13 23:26:57 +0900

    refs #20 Add new project form

--- src/components/projects/new.tsx
+++ src/components/projects/new.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+  Platform,
+  Button,
+} from 'react-native';
 import {ThunkDispatch} from 'redux-thunk';
 import {
   DynamicStyleSheet,
@@ -9,20 +16,129 @@ import {
 import {StackScreenProps} from '@react-navigation/stack';
 
 import {ProjectsParam} from '@/navigations/projects';
+import {useForm, Controller} from 'react-hook-form';
 
 type Props = StackScreenProps<ProjectsParam, 'New'> & {
   dispatch: ThunkDispatch<any, any, any>;
 };
 
-const New: React.FC<Props> = () => {
+type FormData = {
+  title: string;
+  description: string;
+  repository_id?: number;
+};
+
+const New: React.FC<Props> = ({navigation}) => {
+  const {control, handleSubmit, errors} = useForm<FormData>();
+  const onSubmit = handleSubmit(({title, description}) => {
+    console.log(title, description);
+  });
+
+  if (Platform.OS === 'ios') {
+    useLayoutEffect(() => {
+      navigation.setOptions({
+        headerRight: () => <Button onPress={onSubmit} title="Save" />,
+      });
+    });
+  }
+
   const styles = useDynamicValue(dynamicStyles);
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll}>
+        <Text style={styles.label}>Title</Text>
+        <Controller
+          control={control}
+          render={({onChange, onBlur, value}) => (
+            <TextInput
+              style={styles.title}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+              autoFocus={true}
+            />
+          )}
+          name="title"
+          rules={{required: true}}
+          defaultValue=""
+        />
+        {errors.title && <Text style={styles.error}>Title is required.</Text>}
+
+        <Text style={styles.label}>Description</Text>
+        <Controller
+          control={control}
+          render={({onChange, onBlur, value}) => (
+            <TextInput
+              multiline
+              style={styles.description}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="description"
+          defaultValue=""
+        />
+      </ScrollView>
+      {Platform.OS != 'ios' && (
+        <View style={styles.submit}>
+          <Button title="Submit" color="#0069d9" onPress={onSubmit} />
+        </View>
+      )}
+    </View>
+  );
 };
 
 const dynamicStyles = new DynamicStyleSheet({
   container: {
     flex: 1,
     backgroundColor: new DynamicValue('#f0f0f0', '#000000'),
+  },
+  scroll: {
+    marginBottom: 48,
+  },
+  label: {
+    color: new DynamicValue('#000000', '#dcdcdc'),
+    fontSize: 18,
+    marginTop: 12,
+    marginLeft: 16,
+  },
+  title: {
+    borderWidth: 0,
+    color: new DynamicValue('#000000', '#dcdcdc'),
+    backgroundColor: new DynamicValue('#ffffff', '#202020'),
+    marginTop: 8,
+    paddingTop: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingBottom: 12,
+    fontSize: 18,
+    lineHeight: 24,
+    minHeight: 40,
+  },
+  description: {
+    borderWidth: 0,
+    color: new DynamicValue('#000000', '#dcdcdc'),
+    backgroundColor: new DynamicValue('#ffffff', '#202020'),
+    marginTop: 8,
+    paddingTop: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingBottom: 12,
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  error: {
+    color: '#dc3545',
+    marginLeft: 12,
+  },
+  submit: {
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    position: 'absolute',
+    height: 36,
+    bottom: 0,
   },
 });
 
